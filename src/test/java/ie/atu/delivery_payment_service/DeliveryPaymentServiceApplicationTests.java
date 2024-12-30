32d967b407906c8921e1cbe8092cf2b0ce8fc974
SeanC <G00402034@atu.ie>
2024-12-30 17:11:51 +0000

    initial commit

---
+++ src/test/java/ie/atu/delivery_payment_service/DeliveryPaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.delivery_payment_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryPaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
